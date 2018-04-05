4c144e2a1ad58b80480fadd342f022cd60d953d0
chovanec35 <jozefcchovanec@gmail.com>
2018-04-05 21:52:05 +0200

    Odstranenie komentarov z UserDao

--- Projekt/src/main/java/sk/zadanie/dao/UserDao.java
+++ Projekt/src/main/java/sk/zadanie/dao/UserDao.java
@@ -8,18 +8,11 @@ import sk.zadanie.model.Login;
 import sk.zadanie.model.User;
 
 public interface UserDao {
-   /* public void add(User user);
-    public void edit(User user);
-    public void delete(int id);
-    public User getUser(int id);
-    public List getAllUser();*/
     
     public void addNewContact(ContactDto contact, UserDto userDto, int userId);
     public void registration(UserDto user);
     User validateUser(Login login);
     public List<Map<String, Object>> getAllContacts(int userId); //, String fName, String lName, String role
-//    User validateName(Login login);
-//    User validatePassword(Login login);
-//    User validateEmail(Login login);
+
 
 }
